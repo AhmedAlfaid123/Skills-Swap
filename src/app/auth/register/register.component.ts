94104b9dbd24d5a61091873fd6497fc68c18b520
Geneviave <geneviave.medhat@gmail.com>
2026-07-25 00:37:04 +0300

    Create login and register UI

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
-
+import { RouterLink , RouterLinkActive } from "@angular/router";
 @Component ({
     selector:'app-register',
+    imports : [RouterLink , RouterLinkActive], 
     templateUrl: './register.component.html',
     styleUrl: './register.component.css'
 })
