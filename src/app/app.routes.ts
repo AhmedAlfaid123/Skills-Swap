77a5c14c3a4406a50959d788712a03b8c98bd967
Marina Shenouda <Marina.Suleiman.2230143@CIT.hnu.edu.eg>
2026-07-27 20:55:24 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 import { homeComponent } from './home/home.component';
 import { loginComponent } from './auth/login/login.component';
 import { registerComponent } from './auth/register/register.component';
-
+import { MatchingComponent } from './features/matching/matching.component';
+import { RequestsComponent } from './features/requests/requests.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: homeComponent },
   { path: 'register', component: registerComponent },
-  { path: 'login', component: loginComponent }
+  { path: 'login', component: loginComponent },
+  { path: 'matching', component: MatchingComponent },
+  { path: 'requests', component: RequestsComponent }
 ];
+
+
+
+
