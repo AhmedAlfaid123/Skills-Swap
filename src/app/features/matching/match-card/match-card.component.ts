216d42ed387f223fe05202128c6043278c00ee3a
Marina Shenouda <Marina.Suleiman.2230143@CIT.hnu.edu.eg>
2026-08-09 13:21:13 +0300

    Update match-card.component.ts

--- src/app/features/matching/match-card/match-card.component.ts
+++ src/app/features/matching/match-card/match-card.component.ts
@@ -1,18 +1,38 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatchViewModel } from '../../../models/match';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatchRequestSelection, MatchViewModel } from '../../../models/match';
 
 @Component({
   selector: 'app-match-card',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './match-card.component.html',
   styleUrl: './match-card.component.css'
 })
-export class MatchCardComponent {
+export class MatchCardComponent implements OnChanges {
   @Input({ required: true }) match!: MatchViewModel;
   @Output() viewProfile = new EventEmitter<string>();
-  @Output() sendSwapRequest = new EventEmitter<MatchViewModel>();
+  @Output() sendSwapRequest = new EventEmitter<MatchRequestSelection>();
+
+  protected selectedTeachSkillId = '';
+  protected selectedLearnSkillId = '';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['match']) {
+      this.selectedTeachSkillId = this.match.learnSkillOptions[0]?.id ?? '';
+      this.selectedLearnSkillId = this.match.teachSkillOptions[0]?.id ?? '';
+    }
+  }
 
   get avatarLabel(): string {
     return this.match.userName.charAt(0).toUpperCase();
+  }
+
+  sendRequest(): void {
+    this.sendSwapRequest.emit({
+      match: this.match,
+      teachSkillId: this.selectedTeachSkillId,
+      learnSkillId: this.selectedLearnSkillId
+    });
   }
 }
